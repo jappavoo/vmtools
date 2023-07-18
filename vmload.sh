87347e78bf301dfe798cf6aeadbfc7a9fe980692
Jonathan Appavoo <jappavoo@bu.edu>
2023-07-18 11:53:48 -0400

    added nvram archiveing

--- vmload.sh
+++ vmload.sh
@@ -5,6 +5,7 @@ ARCH=$(uname -i)
 DOMAIN=${VMBASEVM:-basevm-$ARCH}
 DISKDIR=${VMDISKDIR:-disk}
 DISK=${DISKDIR}/${DOMAIN}.qcow2
+NVRAM=${DISKDIR}/${DOMAIN}_VARS.fd
 URLBASE=${VMURLBASE:-http://www.cs.bu.edu/~jappavoo/Resources}
 
 
@@ -18,7 +19,8 @@ tar xf ${DOMAIN}.tar
 
 echo "rewriting $DOMAIN" > /dev/stderr
 mv ${DISKDIR}/${DOMAIN}.xml ${DISKDIR}/${DOMAIN}.xml.orig
-sed -e "s%<source file='.*'%<source file='$(pwd)/${DISK}'%" < ${DISKDIR}/${DOMAIN}.xml.orig  > ${DISKDIR}/${DOMAIN}.xml
+sed -e "s%<source file='.*'%<source file='$(pwd)/${DISK}'%" \
+    -e "s%<nvram>.*</nvram>%<nvram>$(pwd)/${NVRAM}</nvram>%" < ${DISKDIR}/${DOMAIN}.xml.orig  > ${DISKDIR}/${DOMAIN}.xml
 
 echo "defining $DOMAIN" > /dev/stderr
 virsh define --file disk/${DOMAIN}.xml
--- vmsave.sh
+++ vmsave.sh
@@ -15,6 +15,10 @@ virsh shutdown $DOMAIN
 echo "dumping xml for $DOMAIN" > /dev/stderr
 virsh dumpxml $DOMAIN > ${DISKDIR}/$DOMAIN.xml
 
+# brittle but will do
+echo "attempting to save nvram" > /dev/stderr
+virsh vol-download --sparse --pool nvram basevm-aarch64_VARS.fd --file disk/${DOMAIN}_VARS.fd
+
 #echo "copying disk for $DOMAIN" > /dev/stderr
 #cp --sparse=always ${DISK} ${SAVEDIR}/
 
